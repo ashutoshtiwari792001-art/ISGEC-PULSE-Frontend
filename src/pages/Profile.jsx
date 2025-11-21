ae9c6385dc9f8b63282eb1def5a4a9ce8a239c92
ashutoshtiwari792001-art <ashutoshtiwari792001@gmail.com>
2025-11-21 13:53:01 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,1 +1,18 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import { Typography } from '@mui/material'
+
+export default function Profile(){
+  return (
+    <div className="app-shell">
+      <Sidebar />
+      <div className="main">
+        <div className="header"><Typography variant="h6">Profile</Typography></div>
+        <div style={{marginTop:12}}>
+          <div className="card">Profile details and settings here</div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
