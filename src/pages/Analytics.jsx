8d9b5f88975a0c5dab5eccd305ad949ae4d78df9
ashutoshtiwari792001-art <ashutoshtiwari792001@gmail.com>
2025-11-21 13:52:43 +0530

    Update Analytics.jsx

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,1 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import api from '../services/api'
+import { Typography } from '@mui/material'
+import Chart from '../components/Chart'
+
+export default function Analytics(){
+  const [data,setData]=useState(null)
+  useEffect(()=>{ api.get('/analytics/trends').then(r=>setData(r.data)).catch(()=>{}) },[])
+  return (
+    <div className="app-shell">
+      <Sidebar />
+      <div className="main">
+        <div className="header"><Typography variant="h6">Analytics</Typography></div>
+        <div style={{marginTop:12}}>
+          <div className="card" style={{padding:12}}>
+            <Chart />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
