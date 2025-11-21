a6265aed4f9f1b6d542b505ed690fb9e4f005e2d
ashutoshtiwari792001-art <ashutoshtiwari792001@gmail.com>
2025-11-21 13:49:58 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,16 @@
+import axios from 'axios'
+import { API_BASE_URL } from '../config'
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: { 'Content-Type': 'application/json' }
+})
+
+api.interceptors.request.use(cfg => {
+  const t = localStorage.getItem('token')
+  if (t) cfg.headers.Authorization = `Bearer ${t}`
+  return cfg
+})
+
+export default api
 
