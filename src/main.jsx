e065ced3542eac0b62bc64b8e65322dd288b4f2a
ashutoshtiwari792001-art <ashutoshtiwari792001@gmail.com>
2025-11-21 13:21:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { CssBaseline } from '@mui/material'
+import './styles/global.css'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CssBaseline />
+    <App />
+  </React.StrictMode>
+)
 
