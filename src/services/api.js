1ad283655b513ff4ceca6938f9330c5d508b8083
ashutoshtiwari792001-art <ashutoshtiwari792001@gmail.com>
2025-11-26 10:42:03 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
-// src/services/api.js
 import axios from "axios";
-import { API_BASE_URL } from "../config";
+import { API_BASE_URL } from "../config.js";
 
 const api = axios.create({
-  baseURL: `${API_BASE_URL}/api`,
+  baseURL: API_BASE_URL,
   headers: { "Content-Type": "application/json" },
 });
 
